0961227f3b483f26ec318d4a10493809fd67ec9f
Michael Sendker <m@standingwater.io>
2024-06-16 15:42:56 -0400

    Wow. We have a parser. Chapter 6

--- jlox/com/craftinginterpreters/lox/AstPrinter.java
+++ jlox/com/craftinginterpreters/lox/AstPrinter.java
@@ -36,21 +36,12 @@ class AstPrinter implements Expr.Visitor<String> {
 
         builder.append("(").append(name);
         for (Expr expr : exprs) {
+            builder.append(" ");
             builder.append(expr.accept(this));
         }
         builder.append(")");
 
         return builder.toString();
-    }
-
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
-                new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(
-                        new Expr.Literal(45.67)));
     }
 
 }
