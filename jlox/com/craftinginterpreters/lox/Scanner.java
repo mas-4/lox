1f43bd48d9c670557a48a69da6050514f6e1b2fd
Michael Sendker <m@standingwater.io>
2024-06-16 09:25:35 -0400

    Fix a bug and autoformat

--- jlox/com/craftinginterpreters/lox/Scanner.java
+++ jlox/com/craftinginterpreters/lox/Scanner.java
@@ -1,11 +1,10 @@
 package com.craftinginterpreters.lox;
 
+import static com.craftinginterpreters.lox.TokenType.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.craftinginterpreters.lox.TokenType.*;
 
 public class Scanner {
     private final String source;
@@ -17,23 +16,23 @@ public class Scanner {
     private static final Map<String, TokenType> keywords;
 
     static {
-      keywords = new HashMap<>();
-      keywords.put("and",    AND);
-      keywords.put("class",  CLASS);
-      keywords.put("else",   ELSE);
-      keywords.put("false",  FALSE);
-      keywords.put("for",    FOR);
-      keywords.put("fun",    FUN);
-      keywords.put("if",     IF);
-      keywords.put("nil",    NIL);
-      keywords.put("or",     OR);
-      keywords.put("print",  PRINT);
-      keywords.put("return", RETURN);
-      keywords.put("super",  SUPER);
-      keywords.put("this",   THIS);
-      keywords.put("true",   TRUE);
-      keywords.put("var",    VAR);
-      keywords.put("while",  WHILE);
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
     }
 
     Scanner(String source) {
@@ -138,23 +137,24 @@ public class Scanner {
         while (isAlphaNumeric(peek())) advance();
         String text = source.substring(start, current);
         TokenType type = keywords.get(text);
-        if (type == null) type == IDENTIFIER;
+        if (type == null) type = IDENTIFIER;
         addToken(type);
     }
 
     private void number() {
-        while (isDigit(peek())) advance();
+        while (isDigit(peek()))
+            advance();
 
         // look for fractional
         if (peek() == '.' && isDigit(peekNext())) {
             // consume the "."
             advance();
 
-            while (isDigit(peek())) advance();
+            while (isDigit(peek()))
+                advance();
         }
 
-        addToken(NUMBER,
-            Double.parseDouble(source.substring(start, current)));
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
 
     }
 
@@ -195,14 +195,15 @@ public class Scanner {
     }
 
     private char peekNext() {
-        if (current + 1 >= source.length()) return '\0';
+        if (current + 1 >= source.length())
+            return '\0';
         return source.charAt(current + 1);
     }
 
     private boolean isAlpha(char c) {
         return (c >= 'a' && c <= 'z') ||
                 (c >= 'A' && c <= 'Z') ||
-                c == '_'
+                c == '_';
     }
 
     private boolean isAlphaNumeric(char c) {
