f596f9c81427af0fecd2a7cdfbcd877ac81e18bf
Michael Sendker <m@standingwater.io>
2024-06-21 16:32:35 -0400

    Hack in closures Ch. 10 done.

--- jlox/com/craftinginterpreters/lox/LoxFunction.java
+++ jlox/com/craftinginterpreters/lox/LoxFunction.java
@@ -3,13 +3,20 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
-    LoxFunction(Stmt.Function declaration) {
+    private final Environment closure;
+    LoxFunction(Stmt.Function declaration, Environment closure) {
         this.declaration = declaration;
+        this.closure = closure;
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        // shouldn't this be a copy? Is it not possible to modify this?
+        // that could be some cool dynamic programming though...
+        //  Alas, in our rush to cram closures in, we have let a tiny bit of dynamic scoping leak into the interpreter.
+        //  In the next chapter, we will explore deeper into lexical scope and close that hole.
+        // Uhh... That's kinda cool though. Why not.
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i)); // c😎😎😎l
         }
