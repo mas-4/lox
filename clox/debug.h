ea2f08f1105746a79ac37cc8e5d79510622e47fb
Michael Sendker <m@standingwater.io>
2024-06-23 11:19:18 -0400

    Chapter 14 disassembler hello world

---
+++ clox/debug.h
@@ -0,0 +1,13 @@
+//
+// Created by malan on 6/23/2024.
+//
+
+#ifndef clox_debug_h
+#define clox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif //clox_debug_h
